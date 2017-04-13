fd4f2aaffce815c25fc4461f9cd37ee1c713646a
Cheeray Huang <cheeray.huang@gmail.com>
2017-04-13 16:15:02 +0800

    init beego framework.

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	_ "chat_server/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+
+	beego.Run(":5001")
+}
